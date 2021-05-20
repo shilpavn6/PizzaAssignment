120b4a2b2330335ea19c6805d404b7aeb3a34b1c
SHILPA <shilpa.shilpa@dxc.com>
2021-05-20 15:36:15 +0530

    modal change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PizzaListComponent } from './components/pizza-list/pizza-list.component';
 import { HeaderComponent } from './components/header/header.component';
-import {  HttpClientModule } from '@angular/common/http';
-import { PizzaDetailComponent } from './components/pizza-list/pizza-detail/pizza-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 import { PizzaService } from './shared/sevices/pizza.service';
 import { FooterComponent } from './components/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
     PizzaListComponent,
     HeaderComponent,
-    PizzaDetailComponent,
-    FooterComponent
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    
+    NgbModule,
+
   ],
+
   providers: [PizzaService],
   bootstrap: [AppComponent]
 })
--- src/app/components/pizza-list/pizza-list.component.ts
+++ src/app/components/pizza-list/pizza-list.component.ts
@@ -1,61 +1,77 @@
-import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../../shared/sevices/pizza.service';
 import { Pizza } from '../../shared/models/pizza.model';
+import { NgbModal, ModalDismissReasons, NgbModalRef, } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pizza-list',
   templateUrl: './pizza-list.component.html',
   styleUrls: ['./pizza-list.component.css']
 })
-export class PizzaListComponent implements OnInit, OnChanges {
+export class PizzaListComponent implements OnInit {
   allPizzaArray: any = [];
-  showDetailItemModal: boolean = false;
-  
+  closeResult: any;
   pizzaDetailObj: any;
-  constructor(private pizzaService: PizzaService) { }
+  statusDetails = [{
+    id: 0, status: "Order Received"
+  },
+  {
+    id: 1, status: "Preparing"
+  },
+  {
+    id: 2, status: "Ready To Deliver"
+  }
+  ];
+  currentStatus: any;
+  constructor(private pizzaService: PizzaService,
+    private modalService: NgbModal) { }
 
   ngOnInit() {
     this.getPizza();
-
-  }
-  ngOnChanges() {
-
-  }
-  refreshParent(bool: boolean) {
-    this.showDetailItemModal = bool;
   }
   getPizza() {
     this.pizzaService.getPizzas().subscribe(data => {
       console.log(data);
       this.allPizzaArray = data;
-      this.getTotalAmount();
-    });
-
-  }
-  getTotalAmount() {
-    var totalPizzasOrderPrice = 0;
-    this.allPizzaArray.forEach(array => {
-      let pizzaArray = array['pizzasOrdered'];
-      pizzaArray.forEach(element => {
-        totalPizzasOrderPrice += element['price'];
+      var totalPizzasOrderPrice = 0;
+      this.allPizzaArray.forEach(array => {
+        let pizzaArray = array['pizzasOrdered'];
+        pizzaArray.forEach(element => {
+          totalPizzasOrderPrice += element['price'];
+        });
+        array['totalAmount'] = totalPizzasOrderPrice;
       });
-      pizzaArray['totalAmount'] = totalPizzasOrderPrice;
     });
-    // var totalPizzasOrder=0;
-    // let priceArray = this.pizzaDetailObj['pizzasOrdered']
-    // priceArray.forEach(element => {
-    //   totalPizzasOrder += element['price']
-
-    // });
-    // this.pizzaDetailObj['totalAmount'] = totalPizzasOrder;
   }
-
-  viewPizzaDetail(item) {
-    this.showDetailItemModal = true;
-    this.pizzaDetailObj = Object.assign(item);
-
+  viewPizzaDetail(content, pizzaDetail) {
+    this.pizzaDetailObj = Object.assign(pizzaDetail);
+          this.currentStatus = this.statusDetails.find(element => {
+            if (this.pizzaDetailObj['status'] == element['id']) {
+              return element['id'];
+            }
+          });
+          console.log(this.currentStatus);    
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        result => {
+          
+          this.closeResult = `Closed with: ${result}`;
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
-
+  getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
 
 
